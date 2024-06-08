46b44749cf0ec078d4ac354fba6ad9eee500fcd1
RomanFisc <romanfisc@gmail.com>
2024-06-08 18:02:14 -0400

    registration finished, building out login validation

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkBox = document.getElementById("checkbox")
   const form = document.getElementById("registration")
 
+  //setting login form
+  const userLogin = document.getElementById("user-log")
+  const passLogin = document.getElementById("pass-log")
+  const loginForm = document.getElementById("login")
+
   const errorElement = document.getElementById('errorDisplay')
 
   form.addEventListener('submit', (e) => {
@@ -20,23 +25,23 @@ document.addEventListener('DOMContentLoaded', () => {
       messages.push("Username must be at least 4 characters long")
     } else if (!pattern.test(userName.value)) {
       messages.push("Username must not use special characters")
+    } else if (localStorage.getItem("username") === userName.value.toLowerCase()) {
+      messages.push("Username already exists")
     }
 
     //email validation
     if (email.value.trim() === "") {
       messages.push('Email must be filled out');
+    } else if (exampleDomain(email.value)) {
+      messages.push('Email must not be from example.com')
     }
 
     //password validation
     if (passWord.value.trim().length < 12) {
       messages.push("Password must be longer than 12 characters")
-    }
-
-    if (passWord.value.trim() === "password".toLowerCase) {
+    } else if (passWord.value.trim() === "password".toLowerCase) {
       messages.push("Password must not contain the word password")
-    }
-
-    if (passWord.value.trim() === userName.value.trim()) {
+    } else if (passWord.value.trim() === userName.value.trim()) {
       messages.push("Password must not contain username")
     }
 
@@ -58,7 +63,42 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       errorElement.style.display = 'none';
     }
+
+    localStorage.setItem("username", userName.value.toLowerCase())
+    localStorage.setItem("email", email.value.toLowerCase())
+    localStorage.setItem("password", passWord.value)
   })
 
-  ///
+  /////// Creating login validation
+  loginForm.addEventListener('submit',(e) => {
+    let messages = []
+
+    if (userLogin.value.trim().toLowerCase() !== localStorage.getItem("username")) {
+      messages.push("Username doesn't exist")
+    }
+
+    if (passLogin.value.trim() !== localStorage.getItem("password")){
+      messages.push("Wrong password")
+    }
+
+    if (messages.length > 0) {
+      e.preventDefault();
+      errorElement.innerHTML = messages.join('<br>');
+      errorElement.style.display = 'block';
+    } else {
+      errorElement.style.display = 'none';
+    }
+  })
+
 });
+
+
+function exampleDomain (email) {
+  const exampleDomainPattern = /^[^\s@]+@example\.com$/;
+  return exampleDomainPattern.test(email.toLowerCase()); 
+}
+
+console.log(localStorage.getItem("username"))
+console.log(localStorage.getItem("email"))
+console.log(localStorage.getItem("password"))
+
